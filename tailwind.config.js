36dd8f48ac88b7157926c4529e6437675b1c3f61
Arbaz Karimi <arbazkarimi@gmail.com>
2024-09-29 18:16:22 +0530

    update my code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'move1': {
+          '0%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-10px)' },
+          '100%': { transform: 'translateY(0px)' },
+        },
+        'blink': {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+      animation: {
+        'move1': 'move1 1.5s ease-in-out infinite',
+        'blink': 'blink 1.5s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
